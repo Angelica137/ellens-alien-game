a61f10ba0c4eac04ca171be49a23c960ef9970e4
Angelica <Angelica137@users.noreply.github.com>
2022-05-02 18:35:39 +0100

    a.health is 3, green

--- scripts/aliens.py
+++ scripts/aliens.py
@@ -1,4 +1,6 @@
 class Alien:
+    health = 3
+
     def __init__(self, x_coordinate: int, y_coordinate: int):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
--- tests/test_aliens.py
+++ tests/test_aliens.py
@@ -5,4 +5,8 @@ def test_alien_has_x_y_coordinates():
     a = Alien(2, 0)
     assert a.x_coordinate == 2
     assert a.y_coordinate == 0
+
+
+def test_alien_hhealth_is_3():
+    a = Alien(2, 0)
     assert a.health == 3
