4104ffa14ad2d4a63c9ae4c0ca8d382ed7788132
Angelica <Angelica137@users.noreply.github.com>
2022-05-02 19:30:21 +0100

    a.hit() reduced health by 1, green

--- scripts/aliens.py
+++ scripts/aliens.py
@@ -4,3 +4,6 @@ class Alien:
     def __init__(self, x_coordinate: int, y_coordinate: int):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
+
+    def hit(self):
+        self.health -= 1
