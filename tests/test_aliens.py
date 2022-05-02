52b8aa66e7b49b9c5be2dcaa81f364823db2d4fb
Angelica <Angelica137@users.noreply.github.com>
2022-05-02 18:24:12 +0100

    test a has coordinates, red

--- tests/test_aliens.py
+++ tests/test_aliens.py
@@ -4,3 +4,8 @@ from scripts.aliens import *
 def test_instantiate_alien_object():
     a = Alien()
     assert type(a) == Alien
+
+
+def test_alien_has_x_y_coordinates():
+    a = Alien(2, 0)
+    assert a.x_coordinate == 2
